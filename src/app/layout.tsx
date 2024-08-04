11422194c2583e242edf85e198e89194da787103
henri <henri.me@skiff.com>
2024-08-04 22:51:45 +0200

    removed the default boilerplaite and installed nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { ThemeProvider } from "next-themes"
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,7 +15,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider enableSystem attribute="class" disableTransitionOnChange defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
